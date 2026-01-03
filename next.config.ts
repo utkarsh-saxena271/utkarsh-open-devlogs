97fc3cd69059a1a06f299d2f8ada7ec9ccf8c4d6
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-01-03 15:59:14 +0530

    Add MIT license and MDX setup (dependencies + next.config.ts)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
+import nextMDX from "@next/mdx";
 import type { NextConfig } from "next";
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  pageExtensions: ["ts", "tsx", "md", "mdx"]
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
