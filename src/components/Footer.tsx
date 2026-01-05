a666aa2e41fc378c3b77e3ac19a90a8aad276ea5
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-01-05 17:43:36 +0530

    added navbar and footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='absolute bottom-0'>
+        <div>
+            Made with &hearts; by Utkarsh.
+        </div>
+        <div>
+            <a href="">Twitter</a>
+            <a href="">Linkedin</a>
+            <a href="">Github-Repo</a>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <h1>Utkarsh-ODL</h1>
+        </div>
+        <div>
+            <a href="">Twitter</a>
+            <a href="">Linkedin</a>
+            <a href="">Github-Repo</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
