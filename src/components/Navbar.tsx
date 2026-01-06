c2dfd00fc037ce8e8383055d24071b95e8014997
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-01-06 23:57:40 +0530

    gave navbar some structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,24 @@
+import { Roboto, Montserrat } from 'next/font/google'
+import { BsGithub } from 'react-icons/bs'
 
+const roboto = Roboto({
+  subsets:['latin']
+})
+const montserrat = Montserrat({
+  subsets:['latin']
+})
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
+    <div className={`${roboto.className} flex flex-row justify-between items-center bg-green-900 p-3 h-20`}>
+        <div className={`${montserrat.className} font-semibold text-3xl`}>
             <h1>Utkarsh-ODL</h1>
         </div>
-        <div>
+        <div className='flex flex-row justify-between items-center'>
             <a href="">Twitter</a>
             <a href="">Linkedin</a>
             <a href="">Github-Repo</a>
+            <BsGithub/>
         </div>
     </div>
   )
