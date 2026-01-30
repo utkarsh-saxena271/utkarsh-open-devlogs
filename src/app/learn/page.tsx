77a52d95183bd18c1ba7aefe7924a31e5dc72744
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-01-31 00:08:57 +0530

    added coming soon page

---
+++ src/app/learn/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function learnPage(){
+  return (
+    <div className="flex justify-center items-center w-screen h-[80vh]">
+        <div className="text-7xl font-thin">
+            COMING SOON
+        </div>
+    </div>
+  )
+}
