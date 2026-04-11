52b2c8651921fb37c1c14b498a4ca9225013befc
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-04-11 09:07:45 +0530

    add container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Hero from "@/components/home-layout/hero";
+import Container from "@/components/layout/container";
 
 
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen px-4 sm:px-6 lg:px-12 py-12 max-w-xl sm:max-w-2xl md:max-w-3xl lg:max-w-4xl mx-auto  my-3">
-
+    <div className="min-h-screen min-w-screen bg-zinc-950">
+      <Container>
+        <Hero/>
+      </Container>
     </div>
   )
 }
