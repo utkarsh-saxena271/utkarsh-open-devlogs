9892ea47728583190e28c71ec7b479212805b373
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-04-15 11:25:40 +0530

    fixed the container

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -7,7 +7,7 @@ export default function Container(
     }
 ){
     return(
-       <div className={`${className} bg-zinc-950 max-w-xl md:max-w-3xl lg:max-w-6xl mx-auto min-h-screen py-10 px-2`}>
+       <div className={`${className} flex flex-col items-center justify-start gap-37 bg-zinc-950 max-w-xl md:max-w-3xl lg:max-w-6xl mx-auto min-h-screen py-10 lg:py-18 px-2`}>
             {children}
         </div>
     )
