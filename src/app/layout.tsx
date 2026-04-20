125e90475c05d4311d81742cfa358a6d534d9ec3
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-04-20 15:13:01 +0530

    fixed typos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Devium",
@@ -16,13 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" >
-      <body className="antialised bg-black text-zinc-300 selection:text-zinc-950 selection:bg-zinc-50">
+      <body className="antialiased bg-black text-zinc-300 selection:text-zinc-950 selection:bg-zinc-50">
         <div className="min-h-screen flex flex-col">
         
         <Navbar/>
         {children}
         <Footer/>
-        <Analytics mode="production" />
+        <Analytics />
         </div>
       </body>
     </html>
